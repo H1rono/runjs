fbb88b115b81088325ca261ea781770fe0b4201f
H1rono <hronok66@gmail.com>
2024-01-16 07:24:28 +0900

    :sparkles: Write sample file operations in example.js

--- example.js
+++ example.js
@@ -1,2 +1,17 @@
 console.log({ a: "b" });
 console.error("Boom!");
+
+const path = "./log.txt";
+try {
+    const contents = await runjs.readFile(path);
+    console.log("Read from a file", contents);
+} catch (e) {
+    console.error(e);
+}
+
+await runjs.writeFile(path, "Lorem ipsum");
+const contents = await runjs.readFile(path);
+console.log("Read from file", path, "contents:", contents);
+console.log("Remove file");
+runjs.removeFile(path);
+console.log("File removed");
