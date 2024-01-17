1eff250e9b98988c0d0bdd2b8c435abbc6c786b1
H1rono <hronok66@gmail.com>
2024-01-17 11:59:25 +0900

    :sparkles: Write sample fetch code

--- example.js
+++ example.js
@@ -15,3 +15,9 @@ console.log("Read from file", path, "contents:", contents);
 console.log("Remove file");
 runjs.removeFile(path);
 console.log("File removed");
+
+console.log("Hello", "runjs!");
+const content = await runjs.fetch(
+    "https://deno.land/std@0.177.0/examples/welcome.ts",
+);
+console.log("Content from fetch", content);
