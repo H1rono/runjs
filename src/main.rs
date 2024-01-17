b0176d2a9d3809b1baae56ab4420bd33b02712df
H1rono <hronok66@gmail.com>
2024-01-18 06:41:26 +0900

    :sparkles: Receive file path as a CLI argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use bpaf::Bpaf;
+
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options)]
+struct Cli {
+    /// JS file to execute
+    #[bpaf(positional("file"))]
+    pub file: String,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    runjs::run_js("example.js").await?;
+    let cli = cli().run();
+    runjs::run_js(&cli.file).await?;
     Ok(())
 }
