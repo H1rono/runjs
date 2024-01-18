fbcf93f7ee6fe50b6d188cc1c64c5b2ebf0df195
H1rono <hronok66@gmail.com>
2024-01-19 05:29:42 +0900

    :sparkles: Create snapshot of runtime.js

--- build.rs
+++ build.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::path::PathBuf;
+
+use deno_core::snapshot_util::{create_snapshot, CreateSnapshotOptions};
+
 fn main() {
-    println!("cargo:rerun-if-changed=src/runtime.js");
+    // Create the runjs extension.
+    let js_files = vec![deno_core::ExtensionFileSource {
+        specifier: "ext:src/runtime.js",
+        code: deno_core::ExtensionFileSourceCode::IncludedInBinary(include_str!("src/runtime.js")),
+    }]
+    .into();
+    let runjs_extension = deno_core::Extension {
+        name: "runjs",
+        js_files,
+        ..Default::default()
+    };
+
+    // Build the file path to the snapshot.
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let snapshot_path = out_dir.join("RUNJS_SNAPSHOT.bin");
+
+    // Create the snapshot.
+    let snapshot_output = create_snapshot(CreateSnapshotOptions {
+        cargo_manifest_dir: env!("CARGO_MANIFEST_DIR"),
+        snapshot_path,
+        extensions: vec![runjs_extension],
+        startup_snapshot: None,
+        skip_op_registration: false,
+        compression_cb: None,
+        with_runtime_cb: None,
+    });
+    for file_path in snapshot_output.files_loaded_during_snapshot {
+        println!("cargo:rerun-if-changed={}", file_path.display());
+    }
 }
