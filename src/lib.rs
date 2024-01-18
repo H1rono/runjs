9004880de6176260334a9b827736c9746838382b
H1rono <hronok66@gmail.com>
2024-01-19 05:33:36 +0900

    :sparkles: Use snapshot

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,18 @@ deno_core::extension!(
     ]
 );
 
+static RUNTIME_SNAPSHOT: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/RUNJS_SNAPSHOT.bin"));
+
 pub async fn run_js(file_path: &str) -> anyhow::Result<()> {
     let current_dir = std::env::current_dir()?;
     let main_module = deno_core::resolve_path(file_path, &current_dir)?;
 
     let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
         module_loader: Some(std::rc::Rc::new(ts_loader::TsModuleLoader)),
+        startup_snapshot: Some(deno_core::Snapshot::Static(RUNTIME_SNAPSHOT)),
         extensions: vec![runjs_extension::ext()],
         ..Default::default()
     });
-    let runtimejs_src = deno_core::FastString::Static(include_str!("runtime.js"));
-    js_runtime.execute_script("[runjs:runtime.js]", runtimejs_src)?;
 
     let mod_id = js_runtime.load_main_module(&main_module, None).await?;
     let result = js_runtime.mod_evaluate(mod_id);
